4cb9eaa42216c70c6ed898af0d831a089dc8f247
Lee <leedo@zaphod.servercentral.net>
2009-04-17 10:06:20 -0500

    Add a config file, begin handling commands

--- colloqueer.pl
+++ colloqueer.pl
@@ -3,34 +3,40 @@
 use strict;
 use warnings;
 
+use FindBin;
+use lib "$FindBin::Bin/lib";
 use Template;
 use DateTime;
 use MIME::Base64 qw/encode_base64/;
+use Encode;
+use IPC::Open2;
 use Gtk2 -init;
 use Gtk2::Gdk::Keysyms;
 use Gtk2::WebKit;
 use Glib qw/TRUE FALSE/;
 use XML::LibXML;
 use XML::LibXSLT;
-use FindBin;
-use lib "$FindBin::Bin/lib";
 use POE qw/Component::IRC::State/;
 use POE::Kernel { loop => 'Glib' };
-use Encode;
-use IPC::Open2;
+use YAML::Any;
+use Data::Dumper;
+
+open my $config_fh, '<', 'config.yaml';
+
+my $config = Load(join "\n", <$config_fh>);
+print Dumper $config;
 
 my $url_re = q{\b(s?https?|ftp|file|gopher|s?news|telnet|mailbox):} .
              q{(//[-A-Z0-9_.]+:\d*)?} .
              q{[-A-Z0-9_=?\#:\$\@~\`%&*+|\/.,;\240]+};
 
-my @channels = ('#onebutan','#cockes','#buttes');
-my $nick = 'leedo';
-
 my $irc = POE::Component::IRC->spawn( 
-  nick => $nick,
-  port => 2227,
-  ircname => $nick,
-  server => 'irc.buttes.org',
+  nick => $config->{nick},
+  port => $config->{server}{port},
+  ircname => $config->{ircname},
+  username => $config->{server}{username},
+  password => $config->{server}{password},
+  server => $config->{server}{host},
 ) or die $!;
 
 POE::Session->create(
@@ -42,7 +48,8 @@ POE::Session->create(
     xslt => XML::LibXSLT->new(),
     irc => $irc,
     style => 'Buttesfire',
-    channels => {},
+    nick => $config->{nick},
+    channels => { map {$_ => {}} @{$config->{channels}} },
   },
 );
 
@@ -52,7 +59,7 @@ exit 0;
 sub irc_001 {
   my ($heap, $sender) = @_[HEAP, SENDER];
   my $irc = $sender->get_heap();
-  for (@channels) {
+  for (keys %{$heap->{channels}}) {
     $irc->yield( join => $_);
     setup_tab($heap, $_);
   }
@@ -68,15 +75,20 @@ sub setup_tab {
   $entry->signal_connect("key_press_event", sub {
     my ($widget, $event) = @_;
     if ($event->keyval == $Gtk2::Gdk::Keysyms{Return}) {
-      $irc->yield(privmsg => $channel => $widget->get_text);
-      push @{$heap->{channels}{$channel}{msgs}}, {
-        nick  => $nick,
-        text  => irc2html($widget->get_text),
-        date  => DateTime->now,
-        id    => encode_base64($nick.time)
-      };
-      $widget->set_text('');
-      refresh_channel($channel, $heap);
+      if ($widget->get_text =~ /^\/(.+)/) {
+        handle_command($heap, $1);
+      }
+      else {
+        $irc->yield(privmsg => $channel => $widget->get_text);
+        push @{$heap->{channels}{$channel}{msgs}}, {
+          nick  => $heap->{nick},
+          text  => irc2html($widget->get_text),
+          date  => DateTime->now,
+          id    => encode_base64($heap->{nick}.time)
+        };
+        $widget->set_text('');
+        refresh_channel($channel, $heap);
+      }
     }
   });
   $frame->add($wv);
@@ -85,6 +97,14 @@ sub setup_tab {
   $heap->{notebook}->append_page($paned, $channel);
   $heap->{channels}{$channel}{view} = $wv;
   $heap->{main_window}->show_all;
+}
+
+sub handle_command {
+  my ($heap, $command) = @_;
+  if ($command =~ /^join (.+)/) {
+    $heap->{irc}->yield( join => $1);
+    setup_tab($heap, $1);
+  }
 }
 
 sub irc_public {
@@ -128,7 +148,7 @@ sub format_messages {
   $heap->{tt}->process('message.xml', {
     user  => $from,
     msgs  => \@msgs,
-    self  => $from eq $nick ? 1 : 0,
+    self  => $from eq $heap->{nick} ? 1 : 0,
   }, \(my $message)) or die $!;
   my $doc = $heap->{parser}->parse_string($message,{encoding => 'utf8'});
   my $results = $heap->{style_doc}->transform($doc,
