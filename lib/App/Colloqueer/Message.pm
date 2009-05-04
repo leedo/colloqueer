b70b2741b0368b883db7d0c4746bbd1a7d8b737f
Lee <leedo@zaphod.servercentral.net>
2009-05-04 10:12:48 -0500

    Better URL regex

--- lib/App/Colloqueer/Message.pm
+++ lib/App/Colloqueer/Message.pm
@@ -12,16 +12,12 @@ has 'id' => (isa => 'Str', is => 'ro', lazy => 1, default => sub {
   return 'a'.time;
 });
 
-my $url_re = q{\b(s?https?|ftp|file|gopher|s?news|telnet|mailbox):} .
-             q{(//[-A-Z0-9_.]+:\d*)?} .
-             q{[-A-Z0-9_=?\#:\$\@~\`%&*+|\/.,;\240]+};
-
 has 'html' => (isa => 'Str', is => 'ro', lazy => 1, default => sub {
   my $self = shift;
   my $string = App::Colloqueer::IRC::Formatting->formatted_string_to_html($self->text);
   $string =~ s/\\/\\\\/g;
   $string =~ s/\s{2}/ &#160;/g;
-  $string =~ s@($url_re+)@<a href="$1">$1</a>@gi;
+  $string =~ s/(https?\:\/\/.+)([\b<])/<a href="$1">$1<\/a>$2/gi;
   return $string;
 });
 
